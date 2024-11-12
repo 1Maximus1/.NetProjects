ec763abd842a2c416c7ec04b8b6366edea0f39a2
Maks <elohhh2@gmail.com>
2024-11-13 01:53:05 +0200

    Created a console application with input and parameter-passing functionality
    1. Initialized a template-based console application using .NET CLI and verified its execution.
    2. Updated the code to prompt for the student's name and group number via the console and display the information in the terminal.
    3. Added support for passing parameters at runtime. The application now outputs the student's name, surname, and group number provided through command-line arguments.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StudentRequest
+{
+    class StudentInfo
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string groupNumber;
+
+            if (args.Length == 3)
+            {
+                
+                firstName = args[0];
+                lastName = args[1];
+                groupNumber = args[2];
+
+                Console.WriteLine($"Ім'я: {firstName}");
+                Console.WriteLine($"Прізвище: {lastName}");
+                Console.WriteLine($"Номер групи: {groupNumber}");
+            }
+            else
+            {
+                
+                Console.WriteLine("Аргументи не передані, введіть дані вручну.");
+
+                Console.Write("Введіть ім'я: ");
+                firstName = Console.ReadLine();
+
+                Console.Write("Введіть прізвище: ");
+                lastName = Console.ReadLine();
+
+                Console.Write("Введіть номер групи: ");
+                groupNumber = Console.ReadLine();
+
+                Console.WriteLine($"Ім'я: {firstName}");
+                Console.WriteLine($"Прізвище: {lastName}");
+                Console.WriteLine($"Номер групи: {groupNumber}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
